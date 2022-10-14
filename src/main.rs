d14c23b0c950056589d993e81c42207dcd700c19
Michael Huang <michaelh@casual.place>
2022-10-13 17:30:50 -0700

    Add basic echo functionality
    Added clap 4 as a dependency. Created struct to get arguments
    for text and an optional "omit newline" flag.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[arg(help = "Input text", required = true)]
+    text: Vec<String>,
+
+    #[arg(short, long = None, help = "Do not print newline")]
+    new: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let newline = if cli.new { "" } else { "\n" };
+    print!("{}{}", cli.text.join(" "), newline);
 }
