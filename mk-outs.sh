0b26a3094237204a7ccef01490d1a9ecdb5f9208
Michael Huang <michaelh@casual.place>
2022-10-15 23:49:54 -0700

    Modify tests

--- mk-outs.sh
+++ mk-outs.sh
@@ -3,10 +3,10 @@
 OUTDIR="tests/expected"
 [[ ! -d "$OUTDIR" ]] && mkdir -p "$OUTDIR"
 
-echo "Hello there"  > $OUTDIR/hello1.txt
-echo "Hello"  "there" > $OUTDIR/hello2.txt
-echo Hello there > $OUTDIR/hello3.txt
-echo -n "Hello  there" > $OUTDIR/hello1n.txt
-echo -n "Hello"  "there" > $OUTDIR/hello2n.txt
-echo -n Hello there > $OUTDIR/hello3n.txt
+echo Hello there  > $OUTDIR/hello1.txt
+echo "Hello there" > $OUTDIR/hello2.txt
+echo "Hello  there" > $OUTDIR/hello3.txt
+echo -n Hello there > $OUTDIR/hello1n.txt
+echo -n "Hello there" > $OUTDIR/hello2n.txt
+echo -n "Hello  there" > $OUTDIR/hello3n.txt
 
